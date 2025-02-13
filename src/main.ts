89757725e6b1cbe7311aee6618c0d5043ff62a03
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-02-13 16:54:52 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', //teste 
-    methods: ["GET"],
-    credentials: false
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
