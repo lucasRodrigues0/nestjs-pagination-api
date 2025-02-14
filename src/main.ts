1e6f53a7613b9e8a670e391e7bbca27c9f1229d6
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-02-14 12:07:43 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://bucolic-rolypoly-c16e19.netlify.app/',
+    methods: ['GET'],
+    allowedHeaders: ['Content-Type']
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
